c4d2d81f51f7b6c1ee68c1294bfb1698dd292bf2
tatsuyuki <tatsuyuki@program.com.tw>
2019-03-05 09:24:58 +0800

    test

--- lib/src/cinch_generator.dart
+++ lib/src/cinch_generator.dart
@@ -34,10 +34,10 @@ class CinchGenerator extends GeneratorForAnnotation<ApiService> {
     }
     _write.clear();
     var test = classElement.library;
-    _write.write('var t3 = "${test.prefixes[0].enclosingElement}";');
     _write.write('var t2 = "${test.prefixes[0].name}";');
-    _write.write('var t1 = "${test.prefixes[0].unit}";');
-    _write.write('var t4 = "${test.prefixes[0].source}";');
+    _write.write('var t1 = "${test.prefixes[0].librarySource}";');
+    _write.write('var t4 = "${test.prefixes[0].kind}";');
+    _write.write('var t5 = "${test.prefixes[0].library}";');
     _write.write("""
     class _\$${classElement.name} extends Service {
       _\$${classElement.name}({Duration connectTimeout = const Duration(seconds: 5), 
